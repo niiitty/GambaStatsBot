d74cf925adeca4d0f9c204df1f84a9dfcf7f07f9
niiitty <niko.typpo@protonmail.com>
2026-08-04 15:17:47 +0300

    Refactor messages

--- src/commands.py
+++ src/commands.py
@@ -3,8 +3,16 @@
 from loguru import logger
 from telegram import Update
 from telegram.ext import ContextTypes
+from telegram.constants import ParseMode
 
 from src.db import add_user, get_stats
+from src.messages import (
+    HELP_MESSAGE,
+    BEGIN_TRACKING,
+    ALREADY_TRACKING,
+    stats_message,
+    NO_STATS_YET,
+)
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -14,15 +22,8 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         return
 
     await message.reply_text(
-        r"""
-🎰 Komennot:
-
-/help \- Tulosta tämä viesti\.
-
-/begin \- Ala seuraamaan voittoja ja häviöitä\.
-/stats \- Tulosta tilastosi\.
-        """,
-        parse_mode="MarkdownV2",
+        text=HELP_MESSAGE,
+        parse_mode=ParseMode.HTML,
     )
 
 
@@ -37,21 +38,11 @@ async def begin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = user.id
     chat_id = chat.id
 
-    response = await add_user(user_id, chat_id)
+    response: bool = await add_user(user_id, chat_id)
     if response:
-        await message.reply_text("🎰 _Gamba gamba_", parse_mode="MarkdownV2")
+        await message.reply_text(text=BEGIN_TRACKING, parse_mode=ParseMode.HTML)
     else:
-        await message.reply_text(
-            r"🎰 _Pyöräytyksesi lasketaan jo\._", parse_mode="MarkdownV2"
-        )
-
-
-def _win_percentage(wins: int, losses: int) -> str:
-    if wins + losses <= 0:
-        return "N/A"
-
-    perc = str(round(wins / (wins + losses), 2))
-    return perc.replace(".", r"\.")
+        await message.reply_text(text=ALREADY_TRACKING, parse_mode=ParseMode.HTML)
 
 
 async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -65,22 +56,18 @@ async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = user.id
     chat_id = chat.id
 
+    if user.username is None or chat.title is None:
+        return
+
     stats = await get_stats(user_id, chat_id)
     if stats:
         wins, losses = stats
         await message.reply_text(
-            rf"""
-🎰*{user.username}* tilastot ryhmässä {chat.title}:
-
-🏆 Voitot: {wins}
-💸 Häviöt: {losses}
-
-🍒 Voitto\-%: {_win_percentage(wins, losses)} %
-            """,
-            parse_mode="MarkdownV2",
+            text=stats_message(user.username, chat.title, wins, losses),
+            parse_mode=ParseMode.HTML,
         )
     else:
         await message.reply_text(
-            r"🎰 _Seuraa ensin pyöräytyksiä komennolla \/begin\._",
-            parse_mode="MarkdownV2",
+            text=NO_STATS_YET,
+            parse_mode=ParseMode.HTML,
         )
